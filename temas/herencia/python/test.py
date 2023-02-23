bc01ec4558f1ad8120a6d9996a0082bb3e32cd9d
Violeta Ai <violetaai.naharrozaldivar@alum.uca.es>
2023-02-23 17:14:27 +0100

    he añadido programa de prueba

---
+++ temas/herencia/python/test.py
@@ -0,0 +1,8 @@
+# programa que calcula el area de un triangulo
+def main():
+    base = float(input("Base del triangulo: "))
+    altura = float(input("Altura del triangulo: "))
+    area = (base * altura) / 2
+    print("El area del triangulo es: ", area)
+
+main()
